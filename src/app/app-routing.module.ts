41311fe987fb4d68a67809d1b8a3b16854d71449
RonaldoFagundes <ronaldofagundes@gmail.com>
2022-10-16 19:36:13 -0300

    implementando e config angular material e os components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/views/home/home.component';
+import { UserLoginComponent } from './components/views/users/user-login/user-login.component';
+import { UserReadComponent } from './components/views/users/user-read/user-read.component';
+
+const routes: Routes = [
+  
+   {
+    path:'',
+    component:HomeComponent
+   },
+
+   {
+    path:'users',
+    component:UserReadComponent
+   },
+
+   {
+    path:'login',
+    component:UserLoginComponent
+   }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HomeComponent } from './components/views/home/home.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { UserReadComponent } from './components/views/users/user-read/user-read.component';
+import { UserLoginComponent } from './components/views/users/user-login/user-login.component';
+import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,    
+    FooterComponent, 
+    HomeComponent, UserReadComponent, UserLoginComponent   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
